1f926d3a60bc0fc5a54442d4789096c198716591
afodo92 <78488198+afodo92@users.noreply.github.com>
2024-09-27 12:28:20 +0300

    Create new.py

---
+++ scripts/new.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python 
+
+print("Python script without velocity_manifest.json")
+
+first_value = 84
+second_value = 5
+atribute = 'awesome'
+
+final_value = first_value * second_value
+
+print (final_value)
+print ('iTest is ' + str(atribute) + '.')
